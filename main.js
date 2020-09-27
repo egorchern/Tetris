a677d29871b71a0e1d7efff8c6c1ac048ad298b7
Egor Chernyshev <egorch.formal@gmail.com>
2020-09-27 13:30:40 +0100

    basic foundation work

--- main.js
+++ main.js
@@ -6,13 +6,18 @@ document.addEventListener("DOMContentLoaded", function(ev){
 let canvas;
 let ctx;
 let field;
+let frame_timer;
+let frame_interval = 16.6;
+let test;
 function init(){
     canvas = document.querySelector("#main_canvas");
     ctx = canvas.getContext("2d");
-    field = new game_field(10, 20);
+    field = new game_field(15, 20);
+    frame_timer = setInterval(function(){process_frame()}, frame_interval);
+    test = new game_block("hsl(120, 60%, 50%)", 40, 40);
 }
 
-let frame_interval = 41.6;
+
 
 
 // game_field class
@@ -22,15 +27,61 @@ class game_field{
 
         this.block_width = canvas.width / width;
         this.block_height = canvas.height / height;
+        this.gravity = 1;
+        this.pieces = [];
+        this.draw = function(){
+            alert("draw works");
+        }
+        this.add_piece = function(piece){
+            this.pieces.push(piece);
+        }
     }
 }
+/*
+Patterns:
+
+    1:           2:           3:             4:             5:
+                
+    *           * *          * *              *             *
+    *           * *            * *          * * *           *
+    *                                                       *    
+    *                                                     * *
+
+
+
+
+
+*/
+
 class game_piece{
     constructor(pattern){
-        
+        this.blocks = [];
     }
 }
 
-function process_frame(){
+class game_block{
+    constructor(fill_color, x, y){
+        this.fill_color = fill_color;
+        this.top = y;
+        this.left = x;
+        this.right = x + field.block_width;
+        this.bottom = y + field.block_height;
+        this.draw = function(){
+            ctx.beginPath();
+            ctx.fillStyle = "hsl(0,0%,20%)"
+            ctx.fillRect(this.left, this.top, this.right, this.bottom);
+            ctx.clearRect(this.left + 3, this.top + 3, this.right - 6, this.bottom - 6);
+            ctx.fillStyle = this.fill_color;
+            ctx.fillRect(this.left + 3, this.top + 3, this.right - 6, this.bottom - 6);
+        }
+    }
+}
 
+
+function process_frame(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    test.top += 2;
+    //test.bottom += 1;
+    test.draw();
 }
 
